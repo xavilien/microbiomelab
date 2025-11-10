5daff6faa9d9f6863bfd85439e5a36ec04e7680b
Xavier Lien <xavilien@gmail.com>
2025-11-10 16:48:55 -0500

    fix task 4 and 6

--- microbiome_test.py
+++ microbiome_test.py
@@ -7,13 +7,12 @@ Created on Thu Oct  4 17:32:48 2018
 import alignment
 import random
 import matplotlib.pyplot as plt
+random.seed(11)
 
 
 def Load16SFastA(path, fraction = 1.0, database_size=200, query_size=50):
     # from a file, read in all sequences and store them in a dictionary
     # sequences can be randomly ignored for testing by adjusting the fraction'''
-
-    random.seed(11)
     
     infile = open(path, 'r')
     database = {}
@@ -85,34 +84,50 @@ def AlignmentMatch(sequence, library):
     return best_score, best_match   # stuff only to run when not called via 'import' here
 
 
+# Agreement curve as a function of k-mer size
 def plot_agreement_curve_1(database, queries):
     num_thresholds = 10
     thresholds = [2 * i + 1 for i in range(num_thresholds)]
     overall_scores = []
+
+    best_K = None
+    best_score = None
+
+    agreement_results = {}
+    for k, sequence in queries.items():
+        _, alignment_best_match = AlignmentMatch(sequence, database)
+        agreement_results[k] = alignment_best_match
+
     for K in thresholds:
         print(f"Testing agreement for kmers of length {K}")
         database_kmers = ConvertLibaryToKmerSets(database, K)
         queries_kmers = ConvertLibaryToKmerSets(queries, K)
         scores = []
         for k, sequence in queries.items():
-            _, alignment_best_match = AlignmentMatch(sequence, database)
+            alignment_best_match = agreement_results[k]
             _, kmer_best_match = KmerMatch(queries_kmers[k], database_kmers)
             if kmer_best_match == alignment_best_match:
                 scores.append(1)
             else:
                 scores.append(0)
         
-        overall_scores.append(sum(scores) / len(scores))
+        score = sum(scores) / len(scores)
+        overall_scores.append(score)
+        if best_score is None or score > best_score:
+            best_K = K
+            best_score = score
 
+    plt.figure()
     plt.plot(thresholds, overall_scores, marker='o')
-    plt.xlabel('K (k-mer size)')
-    plt.ylabel('Agreement (fraction matched)')
+    plt.xlabel('K-mer length')
+    plt.ylabel('Agreement')
     plt.title('K-mer vs Alignment Agreement')
     plt.ylim(0, 1)
-    plt.show()
+    out_path = "task4.png"
+    plt.savefig(out_path, bbox_inches='tight', dpi=300)
+    plt.close()
 
-    out_path = "kmer_vs_alignment_agreement.png"
-    plt.gcf().savefig(out_path, bbox_inches='tight', dpi=300)
+    return best_K
 
 def calculateMinimizers(window_size, k, sequence):
     """
@@ -144,10 +159,7 @@ def calculate_database_minimizers(window_size, k, database):
     return database_minimizers
 
 
-def MinimizerMatch(sequence, minimizer, library_minimizers):
-    best_score = -10000000000
-    best_match = None
-
+def MinimizerMatch(sequence, minimizer, library_minimizers, query_kmers, database_kmers, minimum_minimizer_overlap=1):
     """
     sequence : query sequence
     library : list of 16S sequences
@@ -158,58 +170,101 @@ def MinimizerMatch(sequence, minimizer, library_minimizers):
     """
 
     for k in library_minimizers:
-        if len(minimizer.intersection(library_minimizers[k][1])) >= 1:
-            (score, _, _) = alignment.local_align(sequence, library_minimizers[k][0])
-            if best_match is None or score > best_score:
-                best_score = score 
-                best_match = k
-    
-    return best_score, best_match   # stuff only to run when not called via 'import' here
+        if len(minimizer.intersection(library_minimizers[k][1])) >= minimum_minimizer_overlap:
+            return KmerMatch(query_kmers, database_kmers)
 
 
-def plot_agreement_curve_2(database, queries):
-    best_k = 15
+def plot_agreement_curve_2(database, queries, BEST_K=15):
     num_window_sizes = 10
     window_sizes = [15 * i + 20 for i in range(num_window_sizes)]
     overall_scores = []
+
+    # Calculate local align ground truth
+    agreement_results = {}
+    for k, sequence in queries.items():
+        _, alignment_best_match = AlignmentMatch(sequence, database)
+        agreement_results[k] = alignment_best_match
+
+    # Calculate kmers for queries and database based on best_K from previous part
+    database_kmers = ConvertLibaryToKmerSets(database, BEST_K)
+    queries_kmers = ConvertLibaryToKmerSets(queries, BEST_K)
+
     for window_size in window_sizes:
         print(f"Testing agreement for window size of length {window_size}")
-        database_minimizers = calculate_database_minimizers(window_size, best_k, database)
-        queries_minimizers = calculate_database_minimizers(window_size, best_k, queries)
-        database_kmers = ConvertLibaryToKmerSets(database, best_k)
-        queries_kmers = ConvertLibaryToKmerSets(queries, best_k)
+        database_minimizers = calculate_database_minimizers(window_size, BEST_K, database)
+        queries_minimizers = calculate_database_minimizers(window_size, BEST_K, queries)
         scores = []
         for k, sequence in queries.items():
-            _, minimizer_best_match = MinimizerMatch(sequence, queries_minimizers[k][1], database_minimizers)
-            _, kmer_best_match = KmerMatch(queries_kmers[k], database_kmers)
-            if kmer_best_match == minimizer_best_match:
+            _, minimizer_best_match = MinimizerMatch(sequence, queries_minimizers[k][1], database_minimizers, queries_kmers[k], database_kmers)
+            alignment_best_match = agreement_results[k]
+            if minimizer_best_match == alignment_best_match:
                 scores.append(1)
             else:
                 scores.append(0)
         
         overall_scores.append(sum(scores) / len(scores))
 
+    plt.figure()
     plt.plot(window_sizes, overall_scores, marker='o')
     plt.xlabel('Window size')
-    plt.ylabel('Agreement (fraction matched)')
-    plt.title('K-mer vs Alignment Agreement')
+    plt.ylabel('Agreement')
+    plt.title('Window size vs Alignment Agreement')
     plt.ylim(0, 1)
-    plt.show()
+    out_path = "task6.png"
+    plt.savefig(out_path, bbox_inches='tight', dpi=300)
+    plt.close()
 
-    out_path = "kmer_vs_alignment_agreement_2.png"
-    plt.gcf().savefig(out_path, bbox_inches='tight', dpi=300)
+
+def get_incorrect_base(base):
+    bases = ["A", "T", "G", "C"]
+    alternative_bases = [i for i in bases if i != base]
+    return random.choice(alternative_bases)
+
+
+def illumina_mutation(queries):
+    mutated_queries = {}
+
+    for k, seq in queries.items():
+        mutated_seq = []
+        for i in range(min(250, len(seq))):
+            if random.random() > 0.99:
+                mutated_seq.append(get_incorrect_base(seq[i]))
+            else:
+                mutated_seq.append(seq[i])
+
+        mutated_queries[k] = "".join(mutated_seq)
+        
+    return mutated_queries
+
+
+def nanopore_mutation(queries):
+    mutated_queries = {}
+    for k, seq in queries.items():
+        mutated_seq = []
+        for i in range(len(seq)):
+            if random.random() > 0.9:
+                mutated_seq.append(get_incorrect_base(seq[i]))
+            else:
+                mutated_seq.append(seq[i])
+        
+        mutated_queries[k] = "".join(mutated_seq)
+
+    return mutated_queries
 
 
 if __name__ == "__main__":
-   fn = "bacterial_16s_genes.fa"
-   database, queries = Load16SFastA(fn, fraction=0.5, database_size=10, query_size=5)
-   
-   print ("Loaded %d 16s database sequences." % len(database))
-   print ("Loaded %d 16s query sequences." % len(queries))
+    fn = "bacterial_16s_genes.fa"
+    database, queries = Load16SFastA(fn, fraction=0.5, database_size=10, query_size=5)
 
-#    plot_agreement_curve_1(database, queries)
+    print ("Loaded %d 16s database sequences." % len(database))
+    print ("Loaded %d 16s query sequences." % len(queries))
 
-   plot_agreement_curve_2(database, queries)
+    best_K = plot_agreement_curve_1(database, queries)
+    print(f"Best K-mer size is {best_K}")
+    plot_agreement_curve_2(database, queries, BEST_K = best_K)
+
+    illumina_queries = illumina_mutation(queries)
+    nanopore_queries = nanopore_mutation(queries)
    
 
    
